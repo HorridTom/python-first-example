bd5fd51264e2d6ae18b23a05007ef0226d1cb9b3
HorridTom <woodcock.thomas@gmail.com>
2016-05-14 18:29:35 +0100

    Initial commit

---
+++ de.vogella.python.first/src/FirstModule.py
@@ -0,0 +1,20 @@
+'''
+Created on 24 Feb 2016
+
+@author: tw299
+'''
+# Example project
+
+def chicken(a, b):
+    return a + b
+
+
+def addFixedValue(a):
+    y = 2
+    return y + a
+
+
+print (chicken(1, 2))
+print (addFixedValue(4))
+
+print("Hello World!")
